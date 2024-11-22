92dc5df6b580e6effc4eecb0ee578ffb311c5cc2
ComputerScienceWarrior <computersciencewarrior@gmail.com>
2024-11-22 08:38:13 -0700

    adds external styling for Login component

---
+++ components/Login/styles.js
@@ -0,0 +1,25 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+    input: {
+        padding: 10,
+        margin: 10,
+        border: 'solid',
+        borderWidth: 2,
+        borderRadius: 10
+    },
+    loginButton: {
+        padding: 10,
+        margin: 10,
+        backgroundColor: '#000000',
+        borderRadius: 10,
+    },
+    loginButtonText: {
+        color: '#fff',
+        textAlign: 'center',
+        fontWeight: 'bold',
+        letterSpacing: 1
+    }
+})
+
+export default styles;
