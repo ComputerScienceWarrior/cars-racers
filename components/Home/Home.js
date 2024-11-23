c8f730ca15b6e3e28751df29812fe5eac0ca3996
ComputerScienceWarrior <computersciencewarrior@gmail.com>
2024-11-23 15:28:42 -0700

    adds styling to newly added TouchableOpacity components

--- components/Home/Home.js
+++ components/Home/Home.js
@@ -1,12 +1,21 @@
 import React from "react";
-import { View, StyleSheet, Text, Button } from 'react-native';
+import { View, Text, TouchableOpacity, Image, SafeAreaView } from 'react-native';
+import styles from "./styles";
 
 const Home = ({ navigation }) => {
     return (
-        <View>
-            <Button title="Sign Up" onPress={() => navigation.navigate('SignUp')} />
-            <Button title="Login" onPress={() => navigation.navigate('Login')} />
-        </View>
+        <SafeAreaView>
+            <Image
+                style={styles.splashImage}
+                source={{ uri: 'https://media.wired.com/photos/5926c2f38d4ebc5ab806b65e/master/w_2240,c_limit/HotwheelsTA.jpg' }}
+            />
+            <TouchableOpacity style={styles.signUpButton} onPress={() => navigation.navigate('SignUp')}>
+                <Text style={styles.signUpButtonText}>Sign Up</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.loginButton} onPress={() => navigation.navigate('Login')}>
+                <Text style={styles.loginButtonText}>Login</Text>
+            </TouchableOpacity>
+        </SafeAreaView>
     );
 };
 
