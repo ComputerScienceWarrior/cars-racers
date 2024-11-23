301e7610eafb667ea0e3c2ee172e9166ecc5c4e3
ComputerScienceWarrior <computersciencewarrior@gmail.com>
2024-11-23 15:27:35 -0700

    adds styling to homepage

---
+++ components/Home/styles.js
@@ -0,0 +1,31 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+    signUpButton: {
+        backgroundColor: 'orange',
+        margin: 5,
+        borderRadius: 10,
+        padding: 10,
+    },
+    signUpButtonText: {
+        textAlign: 'center',
+        color: 'white',
+    },
+    loginButton: {
+        backgroundColor: 'blue',
+        margin: 5,
+        borderRadius: 10,
+        padding: 10
+    },
+    loginButtonText: {
+        textAlign: 'center',
+        color: 'white',
+    },
+    splashImage: {
+        width: 200,
+        height: 200,
+        margin: 'auto',
+    }
+});
+
+export default styles;
